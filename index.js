f5785377d0752f70607f46d7389ba4c7f1be2d01
Srushti Shete <shetesrushti11@gmail.com>
2023-06-21 20:12:17 +0530

    Open AI backend

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+// Enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/openai', require('./routes/openaiRoutes'));
 
