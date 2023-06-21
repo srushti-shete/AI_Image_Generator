ba607ee988f6743d128aefb90ce3725d551f1498
Srushti Shete <shetesrushti11@gmail.com>
2023-06-21 19:46:46 +0530

    Open AI Library Request/Response

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+
+app.use('/openai', require('./routes/openaiRoutes'));
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
